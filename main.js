7e4767145a4ef7135b45cf72775d3d96ced2fc68
EvadeStorm023 <96498894+EvadeStorm023@users.noreply.github.com>
2022-01-23 10:55:53 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+can= document.getElementById("mycanvas");
+ctx= can.getContext("2d");
+
+ctx.beginPath
+ctx.strokeStyle
+ctx.stroke
+ctx.beginPath();
+ctx.strokeStyle= "blue";
+ctx.lineWidth =6;
+ctx.arc(250,200,100,0, 2*Math.PI)
+ctx.stroke();
+
+ctx.beginPath
+ctx.strokeStyle
+ctx.stroke
+ctx.beginPath();
+ctx.strokeStyle= "black";
+ctx.lineWidth =6;
+ctx.arc(400,200,100,0, 2*Math.PI)
+ctx.stroke();
+
+ctx.beginPath
+ctx.strokeStyle
+ctx.stroke
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth =6;
+ctx.arc(550,200,100,0, 2*Math.PI)
+ctx.stroke();
+
+ctx.beginPath
+ctx.strokeStyle
+ctx.stroke
+ctx.beginPath();
+ctx.strokeStyle= "yellow";
+ctx.lineWidth =6;
+ctx.arc(320,350,100,0, 2*Math.PI)
+ctx.stroke();
+
+ctx.beginPath
+ctx.strokeStyle
+ctx.stroke
+ctx.beginPath();
+ctx.strokeStyle= "green";
+ctx.lineWidth =6;
+ctx.arc(480,350,100,0, 2*Math.PI)
+ctx.stroke();
+
+
+
+
